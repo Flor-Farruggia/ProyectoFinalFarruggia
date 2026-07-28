54962aed1ac5e82cb03b637c8dab3cc83540f5ea
Flor Farru <flor.farr@gmail.com>
2026-07-28 16:47:51 -0300

    Configuración inicial del proyecto con sus archivos

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Proyecto Final Farruggia Prueba</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './assets/css/colors.css';
+import './assets/css/fonts.css';
+import './assets/css/styler.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
